da040df96a1ceb7810e60a59a2d9d372d4a2ac56
dytni <nikolajolesko391@gmail.com>
2024-04-30 16:26:55 +0300

    Start commit

---
+++ src/main/java/by/dytni/finalshop/FinalShopApplication.java
@@ -0,0 +1,13 @@
+package by.dytni.finalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalShopApplication.class, args);
+    }
+
+}
