686a37e475cb430f6b864249479cb738933c3b6d
dytni <nikolajolesko391@gmail.com>
2024-07-05 19:03:07 +0300

    start working with app properties

---
+++ src/main/java/by/dytni/finalshop/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package by.dytni.finalshop.config;
+
+import org.springframework.context.annotation.Configuration;/*
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;*/
+
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfig /* extends WebSecurityConfigurerAdapter */{
+
+   // @Override
+    protected void configure(/*HttpSecurity http*/) throws Exception {
+       /* http
+                .csrf().disable() // Отключите CSRF для тестирования, включите на production
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();*/
+    }
+}
