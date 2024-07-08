defdab5d5da1fcc275fa0e6d9e93be4b6d346752
dytni <nikolajolesko391@gmail.com>
2024-07-09 02:38:46 +0300

    login and properties update

---
+++ src/main/java/by/dytni/finalshop/controller/LoginController.java
@@ -0,0 +1,13 @@
+package by.dytni.finalshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+}
