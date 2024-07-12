0e0650b241d1f9104873820e003b5d9aac038d72
dytni <nikolajolesko391@gmail.com>
2024-07-12 09:25:46 +0300

    README

---
+++ src/main/java/by/dytni/finalshop/config/DataInitializer.java
@@ -0,0 +1,30 @@
+package by.dytni.finalshop.config;
+
+import by.dytni.finalshop.domain.users.User;
+import by.dytni.finalshop.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    public CommandLineRunner initDatabase(UserRepository userRepository) {
+        return args -> {
+            String username = "admin";
+            String password = "admin";
+            String role = "ROLE_ADMIN";
+
+            if (userRepository.findByUsername(username) == null) {
+                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+                User user = new User();
+                user.setUsername(username);
+                user.setPassword(encoder.encode(password));
+                user.setRole(role);
+                userRepository.save(user);
+            }
+        };
+    }
+}
