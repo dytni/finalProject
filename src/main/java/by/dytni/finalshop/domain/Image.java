31b7db7e6308254d7453ff36b4eb511e8ad60bdc
dytni <nikolajolesko391@gmail.com>
2024-07-16 18:55:17 +0300

    Add class Image

---
+++ src/main/java/by/dytni/finalshop/domain/Image.java
@@ -0,0 +1,33 @@
+package by.dytni.finalshop.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "image")
+@Table(name = "image")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "originalFileName")
+    private String originalFileName;
+    @Column(name = "size")
+    private Long size;
+    @Column(name = "contentType")
+    private String contentType;
+    @Column(name = "isPreviewImage")
+    private boolean isPreviewImage;
+    @Lob
+    private byte[] bytes;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+}
